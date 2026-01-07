966bd16e1402f65f3c820d18b5c0061759197bf6
CogsFlow Developer <admin@cogsflow.com>
2026-01-07 20:19:21 +0300

    Replace local Font Awesome with CDN to fix missing webfont 404 errors

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -22,7 +22,7 @@ helper('notification');
     
     <!-- Icons -->
     <link rel="stylesheet" href="<?= base_url('assets/css/boxicons.min.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/css/fontawesome.min.css') ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
     
     <!-- Core CSS -->
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
